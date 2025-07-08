14e7172a4a4c868729b22d6267c00cfadd6a20a7
TennisBowling <tennisbowling@tennisbowling.com>
2025-07-07 19:44:13 -0700

    begin supporting directories and create op

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,22 +1,60 @@
-use bitcode::{Encode, Decode};
+use bitcode::{Decode, Encode};
 
-
-
-#[derive(Encode, Decode, Debug)]
+#[derive(Encode, Decode, Debug, Copy, Clone)]
 pub struct Inode(pub u64);
 
-
 #[derive(Encode, Decode, Debug)]
-pub enum Op {
-    Write { inode: Inode, offset: u64, data: Vec<u8> },
-    Read { inode: Inode, offset: u64, size: u64 },
-    Other(String)
+pub struct Entry {
+    pub name: String,
+    pub inode: Inode,
+    pub is_dir: bool,
 }
 
+// Some thoughts: we store just the file names in the directory but theres no way to go from inode back to filename. should be ok?
+#[derive(Encode, Decode, Debug)]
+pub enum Op {
+    Write {
+        inode: Inode,
+        offset: u64,
+        data: Vec<u8>,
+    },
+    Read {
+        inode: Inode,
+        offset: u64,
+        size: u64,
+    },
+    Create {
+        inode: Inode,
+        parent_inode: Inode,
+        name: String,
+        is_dir: bool,
+    },
+    Delete {
+        inode: Inode,
+    },
+    GetSize {
+        inode: Inode,
+    },
+
+    // Directory ops
+    CreateDir {
+        inode: Inode,
+    },
+    DeleteDir {
+        inode: Inode,
+    },
+    ListDirEntries {
+        inode: Inode,
+    },
+
+    Other(String),
+}
 
 #[derive(Encode, Decode, Debug)]
 pub enum OpResponse {
     WriteOk,
     ReadData(Vec<u8>),
     Error(String),
+
+    CreateOk
 }
